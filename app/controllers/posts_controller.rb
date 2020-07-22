5c4e7eda588aa01a4ea9c5d76506e8d1250ce93b
covalt-ta <taku.covayashi@gmail.com>
2020-07-22 23:38:14 +0900

    コントローラーの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @post = Post.new
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
   end
 
   def create
-    Post.create(content: params[:content])
+    Post.create(content: params[:post][:content])
+    redirect_to action: :index
   end
 
 end
